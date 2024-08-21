eaa4805878e818d20e4fb69517fc7f7f9877bfed
kota1795 <mori.green17@gmail.com>
2024-08-21 16:59:15 +0900

    testing-move

--- nikki/models.py
+++ nikki/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+#クラスを定義
+class Post(models.Model):
+     title = models.CharField(max_length=20)
+     body=models.TextField(max_length=500)
