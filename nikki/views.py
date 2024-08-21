e6860ec7d9f068e85719522a9254ef339905186b
kota1795 <mori.green17@gmail.com>
2024-08-21 17:11:22 +0900

    test-HelloWorld!

--- nikki/views.py
+++ nikki/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "nikki/index.html",{
-        "nikki":nikki
+        "nikki":"nikki"
     })
