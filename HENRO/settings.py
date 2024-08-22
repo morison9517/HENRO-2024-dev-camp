97826fa760c03d7b1fe29189229c489398798642
kota1795 <mori.green17@gmail.com>
2024-08-22 18:31:36 +0900

    .env-is-kasu2

--- HENRO/settings.py
+++ HENRO/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-# settings.py
+
 from decouple import config
 
 LINE_CHANNEL_SECRET = config('LINE_CHANNEL_SECRET')
