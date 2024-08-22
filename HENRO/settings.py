26adbd98463f9fa7cd4f757a1bca54957bc94a73
kota1795 <mori.green17@gmail.com>
2024-08-22 14:49:46 +0900

    file-space

--- HENRO/settings.py
+++ HENRO/settings.py
@@ -12,8 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Quick-start development settings - unsuitable for production
--- nikki/models.py
+++ nikki/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-# Create your models here.
-#クラスを定義
 class Post(models.Model):
-     title = models.CharField(max_length=20)
-     content = models.TextField(max_length=500)
-     created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-def __str__(self):
+    def __str__(self):
         return self.title
+
